677c7c20f5ec546ae1bcee8f242905b42ef476fe
Serchcode <pe280591@uaeh.edu.mx>
2018-10-17 15:23:31 -0500

    host

--- arquitectura/settings.py
+++ arquitectura/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(!k8+=l%cuuzkxzr5mmq0skfq8+1f@uqlh4ey=j6v(47z#u5$p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
