bbe4873d3a8ac04a59b1603a111c7053c1739cbe
Serchcode <pe280591@uaeh.edu.mx>
2018-10-17 21:01:23 -0500

    static url

--- arquitectura/urls.py
+++ arquitectura/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(("main.urls","main"),namespace="main")),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
